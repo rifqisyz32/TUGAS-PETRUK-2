d61a5e9352c08d29ff93c2269afed072d731445d
Affandi09 <48713631+Affandi09@users.noreply.github.com>
2019-04-03 19:16:33 +0700

    Create Linked-List.cpp

---
+++ Linked-List.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <conio.h> 
+
+using namespace std;
+
+int main()
+{
+  cout<<"\t\t\t\t\t    ------------------------ \n";
+	cout<<"\t\t\t\t\t   |  PROGRAM LINKED LIST  |\n";
+	cout<<"\t\t\t\t\t    ------------------------ \n";
+	cout<<"\n\t\t\t\t\t              Oleh\n\n";
+	cout<<"\n\n\t\t\t\t\t     Rifqi Syamsu Zulkarnain\n\t\t\t\t\t\t    1817051032\n\n";
+	cout<<"\n\n\t\t\t\t\t         Rifqi Adiyatma\n\t\t\t\t\t\t    1817051055\n";
+	cout<<"\n\n\t\t\t\t\t      Tommy Ferdiansyah A \n\t\t\t\t\t\t    1817051072\n\n";
+	cout<<"\n\n\t\t\t\t\t     Gilang Rahman Affandi\n\t\t\t\t\t\t    1817051075\n";
+	system ("pause");
+	system ("cls");
+	cout<<"\t\t\t\t\t\t    ----------- \n";
+	cout<<"\t\t\t\t\t\t   | LINKED LIST |\n";
+	cout<<"\t\t\t\t\t\t    ----------- \n";
